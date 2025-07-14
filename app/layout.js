a87d9b193d0faa44f355e8b926b2723ab9f7922c
NabilAmilk <nabil.xxiv@gmail.com>
2025-07-14 20:43:26 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,16 @@
+// app/layout.js
+
+import '../styles/globals.css';
+
 export const metadata = {
   title: 'NexGen AI',
   description: 'Your trusted AI Transformation Partners',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black text-white">{children}</body>
     </html>
-  )
+  );
 }
